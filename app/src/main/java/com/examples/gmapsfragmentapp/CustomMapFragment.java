754c5f6cebf2673876a68cb38619ee789b3a192f
Raphael <jr2004@hotmail.fr>
2017-01-13 21:58:37 +0100

    Example of code & app\build.gradle library import simplification.

---
+++ app/src/main/java/com/examples/gmapsfragmentapp/CustomMapFragment.java
@@ -0,0 +1,19 @@
+package com.examples.gmapsfragmentapp;
+
+import com.google.android.gms.maps.GoogleMap;
+
+/**
+ * Add to be custom
+ */
+
+// TODO Custom your fragment
+public class CustomMapFragment extends BaseMapFragment {
+
+    @Override
+    protected void onMapInitialized() {
+        GoogleMap googleMap = getMap();
+
+        // TODO start doing some code
+
+    }
+}
